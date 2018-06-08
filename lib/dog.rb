ad4808808673fdb97014b112b3713825606973ca
maddiehaferbier <madisonhaferbier@gmail.com>
2018-06-08 17:05:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
-# dog.rb
+class Dog 
+  def name=(dog_name)
+  @this_dogs_name=(dog_name)
+  end
+
+  def name 
+  @this_dogs_name
+  end 
+end
+fido = Dog.new 
+fido.name = "fido"
+
+puts fido.name 
+
+snoopy = Dog.new 
+snoopy.name = "snoopy"
+
+puts snoopy.name 
+
+lassie = Dog.new 
+lassie.name = "lassie"
+
+puts lassie.name 
