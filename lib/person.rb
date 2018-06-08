11b81c567bfb77ab2a8315523a807f1dfb6054d2
maddiehaferbier <madisonhaferbier@gmail.com>
2018-06-08 17:08:42 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,19 @@
-# person.rb
+class Person
+  def name=(person_name)
+  @this_persons_name=(person_name)
+  end
+
+  def name 
+  @this_persons_names_name
+  end 
+end
+adele_goldberg = Person.new 
+adele_goldberg.name = "adele_goldberg"
+
+puts adele_goldberg.name 
+
+alan_kay = Person.new 
+alan_kay.name = "alan_kay"
+
+puts alan_kay.name 
+
